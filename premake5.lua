8ac1e06af4d050af5d17a5439b3bad805ddced70
Siddhant <shindesiddhant709@gmail.com>
2025-02-18 11:47:25 +0530

    Changed premake

--- premake5.lua
+++ premake5.lua
@@ -1,9 +1,15 @@
 project "yaml-cpp"
 	kind "StaticLib"
 	language "C++"
+	staticruntime "on"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	defines
+	{
+		"YAML_CPP_STATIC_DEFINE"
+	}
 
 	files 
 	{
